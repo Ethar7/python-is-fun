2154349311fc5ec35ab5e5dde7aebdd909a5bd26
Ethar7 <nabihethar@yahoo.com>
2023-12-23 04:50:43 -0800

    o

--- ch2pythonbook/1-convert.py
+++ ch2pythonbook/1-convert.py
@@ -3,12 +3,9 @@
 # A program to convert Celsius temps to Fahrenheit
 # by: Susan Computewell
 
-def main():
-    print("This program prints a table of Celsius temperatures and the Fahrenheit equivalents.")
-    print()
-    print("Celsius\tFahrenheit")
-    print("-------------------")
-    for celsius in range(0, 101, 10):
-        fahrenheit = 9/5 * celsius + 32
-        print(f"{celsius}\t{fahrenheit}")
-    main()
+print("Celsius\tFahrenheit")
+print("-------------------")
+for celsius in range(0, 101, 10):
+    fahrenheit = 9/5 * celsius + 32
+    print(f"{celsius}\t{fahrenheit}")
+
